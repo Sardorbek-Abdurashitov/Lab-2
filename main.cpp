e84fc4de88c016670d07d1ef315f8db26388f51a
s.abdurashitov <s.abdurashitov@newuu.uz>
2025-01-24 10:39:54 +0500

    Lab 2 updated

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     //problem1
 
-    // string name;
-    // int age;
-    // cout << " Please, Enter your name: ";
-    // cin >> name ;
-    // cout << "OK" ;
-    // cout << ", Please enter your age" ;
-    // cin >> age ;
-    // cout << "Hello " << name << ", you are " << age << " years old." << endl;
+    string name;
+    int age;
+    cout << " Please, Enter your name: ";
+    cin >> name ;
+    cout << "OK" ;
+    cout << ", Please enter your age" ;
+    cin >> age ;
+    cout << "Hello " << name << ", you are " << age << " years old." << endl;
 
 //problem2
     for (int i = 1; i <= 5; ++i)
