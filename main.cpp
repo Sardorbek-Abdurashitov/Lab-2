08c3839dabaea0f3a515ba8aa95d76b989075aa1
s.abdurashitov <s.abdurashitov@newuu.uz>
2025-01-24 10:32:27 +0500

    z

--- main.cpp
+++ main.cpp
@@ -16,6 +16,28 @@ int main() {
 //problem2
     for (int i = 1; i <= 5; ++i)
         cout << setw(5) << i << endl;
-
+    //problem 3
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    // Swapping the values
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "Swapped values: " << "a = " << a << ", b = " << b << endl;
+ //problem 4
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << setw(10) << "Number" << setw(10) << "Square" << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << setw(10) << i << setw(10) << i * i << endl;
+    }
+    //problem 5
+    string str;
+    cout << "Enter a string: ";
+    cin.ignore(); // To ignore the newline character left in the buffer
+    getline(cin, str);
+    cout << setw(50) << setiosflags(ios::left) << str << endl;
     return 0;
     }
